9f01d6b07a6cf8028eeca53cb8e7128c82440186
Robert Conner <rtconner@smarter.bz>
2019-06-10 22:49:57 -0400

    [save]

--- Dto/InfoDto.php
+++ Dto/InfoDto.php
@@ -2,12 +2,15 @@
 
 namespace DealerInventory\Client\Dto;
 
+use Tightenco\Collect\Support\Collection;
+
 /**
  * @property-read string key
  * @property-read string name
  * @property-read string domain
- * @property-read string active
  * @property-read string sales_email
+ * @property-read string paypal_email
+ * @property-read Collection|LocationDto[] locations
  */
 class InfoDto extends Dto
 {
