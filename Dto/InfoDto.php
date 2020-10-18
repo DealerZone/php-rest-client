d05f4ebfb5dbead7d0ba33e7dee52327ab1c2483
Robert Conner <rtconner@smarter.bz>
2020-10-17 23:19:41 -0400

    [save]

--- Dto/InfoDto.php
+++ Dto/InfoDto.php
@@ -10,6 +10,8 @@ use Tightenco\Collect\Support\Collection;
  * @property-read string domain
  * @property-read string sales_email
  * @property-read string paypal_email
+ * @property-read string sales_phone_number
+ * @property-read string fax_number
  * @property-read Collection|LocationDto[] locations
  */
 class InfoDto extends Base
